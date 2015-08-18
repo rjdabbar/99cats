72c28ad158d78edfe750d817d358dbb675ad44fb
Jake and RJ <jake+rj@gmail.com>
2015-08-18 11:15:51 -0400

    Validates a valid inclusion set

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,7 +1,7 @@
 class Cat < ActiveRecord::Base
   validates :name, presence: true
-  validates :color, inclusion: { in: %w(white, black, calico, orange, tabby, grey)}
-  validates :sex, inclusion: { in: %w(M, F)}
+  validates :color, inclusion: { in: %w(white black calico orange tabby grey)}
+  validates :sex, inclusion: { in: %w(M F)}
 
 
   def age
