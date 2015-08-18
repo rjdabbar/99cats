f32097f3e51bb1629a02783ea97fc19c12f41b9c
Jake and RJ <jake+rj@gmail.com>
2015-08-18 11:40:04 -0400

    Fixed Cat#age

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class Cat < ActiveRecord::Base
   validates :name, presence: true
   validates :color, inclusion: { in: %w(white black calico orange tabby grey)}
@@ -5,6 +6,10 @@ class Cat < ActiveRecord::Base
 
 
   def age
-    self.birth_date - Time.now
+
+  #  debugger
+    # self.birth_date - Time.now.strftime("%Y-%m-%d")
+    ((Time.now.to_date - self.birth_date) / 365.0).round(2)
+
   end
 end
