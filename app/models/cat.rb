2b0c80bb75f82ef1f2ca83ab696be7fa18541d3b
Jake and RJ <jake+rj@gmail.com>
2015-08-18 11:06:28 -0400

    Add gems and create CatsController

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,6 +1,7 @@
 class Cat < ActiveRecord::Base
   validates :name, presence: true
-
+  validates :color, inclusion: { in: %w(white, black, calico, orange, tabby, grey)}
+  validates :sex, inclusion: { in: %w(M, F)}
 
 
   def age
