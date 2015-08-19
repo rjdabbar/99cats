64396fd3af572ae5d180b53ee9eb688f3663844e
Jake and RJ <jake+rj@gmail.com>
2015-08-19 18:01:02 -0400

    Final commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   before_action :check_signed_in, only: [:new, :create]
 
+
   def new
   end
 
@@ -10,16 +11,20 @@ class SessionsController < ApplicationController
       sign_in(@user)
       redirect_to cats_url
     else
+      flash.now[:errors] = @user.errors.full_messages
       render :new
     end
   end
 
   def destroy
-    sign_out(current_user)
+    sign_out
     redirect_to new_session_url
   end
 
+
+
   private
+
 
   def user_params
     params.require(:user).permit(:user_name, :password)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
       sign_in(@user)
       redirect_to cats_url
     else
+      flash.now[:errors] = @user.errors.full_messages
       render :new
     end
   end
